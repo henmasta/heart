b0297d7ade292cb53e1299190e9c4343896a5f3b
Henmasta <39940952+henmasta@users.noreply.github.com>
2023-04-24 18:08:05 +0300

    Update Heart.java

--- Heart.java
+++ Heart.java
@@ -1,6 +1,14 @@
+import java.io.*;
+
 class Heart {
 	public static void main(String args[]) {
-		int n = Integer.parseInt(args[0]);
+		int n;
+		try {
+			n = Integer.parseInt(args[0]);
+		} catch(Exception e){
+			n = 3;
+		}
+
 		for(int i = -3*n/2; i <= n; i++) {
 			for(int j = -3*n/2; j <= 3*n/2; j++) {
 				if ((Math.abs(i) + Math.abs(j) < n) || ((-n/2-i) * (-n/2-i) + (n/2-j) * (n/2-j) <= n*n/2) || ((-n/2-i) * (-n/2-i) + (-n/2-j) * (-n/2-j) <= n*n/2)) {
